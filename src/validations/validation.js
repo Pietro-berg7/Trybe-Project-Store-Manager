df9dd7df38bb782b2d6175a1c1e99d00aaae0130
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-03-12 04:25:32 +0044

    Refatora validation

--- src/validations/validation.js
+++ src/validations/validation.js
@@ -2,21 +2,16 @@ const { idSchema, insertSchema } = require('./schemas');
 
 const idValidate = (id) => {
   const { error } = idSchema.validate(id);
-  if (error) {
-    return { type: 422, message: '"id" must be a number' };
-  }
-  return { type: null, message: 'OK' };
+  return error
+    ? { type: 422, message: '"id" must be a number' }
+    : { type: null, message: 'OK' };
 };
 
 const productValidate = (name) => {
   const { error } = insertSchema.validate(name);
-  if (error) {
-    return {
-      type: 422,
-      message: '"name" length must be at least 5 characters long',
-    };
-  }
-  return { type: null, message: 'OK' };
+  return error
+    ? { type: 422, message: '"name" length must be at least 5 characters long' }
+    : { type: null, message: 'OK' };
 };
 
 module.exports = {
